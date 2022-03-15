88a0a578ef66cbe59a3b5d652a5f12a524fd2e49
Siahmakoun <siahmasf@rose-hulman.edu>
2022-03-15 13:27:11 -0400

    merged attach and clear screen funcs

--- UtilManager.py
+++ UtilManager.py
@@ -3,15 +3,13 @@ class UtilManager:
     def __init__(self, master):
         self.master = master
 
-    def clear_screen(self):
+    @staticmethod
+    def attach_new_screen_to_main(self, widgets):
         for w in self.master.winfo_children():
             w.pack_forget()
 
-    @staticmethod
-    def attach_to_main(widgets, name):
         for w in widgets:
             w.pack()
-        print('On screen ', name)
 
     @staticmethod
     def attach_image_main(self, el):
