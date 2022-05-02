c3db5b84e55936c4dbc32f77b18227b38e450e28
Siahmakoun <siahmasf@rose-hulman.edu>
2022-05-02 09:53:39 -0400

    PDF output functioning, fixing formats now

--- FinalScreen.py
+++ FinalScreen.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
-from fpdf import FPDF
-import ScreenManager as sm
+
+from reportlab.lib.pagesizes import *
+from reportlab.pdfgen import canvas
 
 
 def create_page(master):
@@ -30,4 +31,26 @@ def popup_check():
     yes_btn.grid(row=1, column=0, sticky=tk.E, padx=15)
     no_btn = tk.Button(pop, text='NO', bg='red', command=pop.destroy)
     no_btn.grid(row=1, column=1, sticky=tk.W)
+
+
+def create_assessment_report(images, adjustments, text_boxes):
+    page = canvas.Canvas("Reba Assessment Report.pdf", pagesize=(A4[1], A4[0]))
+    page.setFont('Times-Roman', 12)
+    spaces = 0
+    x_cord = 30
+    f = open("reba-output.txt", "r")
+    for line in f:
+        if line == '\n':
+            spaces += 1
+        elif spaces > 33:
+            x_cord = 380
+            spaces = 0
+            page.drawString(380, 500 - (spaces*10), line)
+        else:
+            page.drawString(x_cord, 500 - (spaces * 10), line)
+            spaces += 1
+    page.save()
+
+
+create_assessment_report(0, 0, 0)
 
