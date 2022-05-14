2fdd6743b7cb5ae5cf907635ec04c785b173cb4a
Siahmakoun <siahmasf@rose-hulman.edu>
2022-05-14 19:23:11 -0400

    All DataFrames functioning properly

--- UserInputManager.py
+++ UserInputManager.py
@@ -1,13 +1,13 @@
 import pandas as pd
 from abc import ABC, abstractmethod
-import pickle as p
+import json as js
 import os
 
 
 class TableManager(ABC):
 
     @abstractmethod
-    def get_tableA_score(self, score_1, score_2, score_3):
+    def get_tableA_score(self, score_1, score_2, score_3, op_score_4):
         pass
 
     @abstractmethod
@@ -30,7 +30,7 @@ class RebaTableManager(TableManager):
         self.tableB = self.create_table(2)
         self.tableC = self.create_table(3)
 
-    def get_tableA_score(self, neck_score, trunk_score, leg_score):
+    def get_tableA_score(self, neck_score, trunk_score, leg_score, filler):
         computed_value = self.tableA[trunk_score][neck_score]
         computed_value = computed_value[str(leg_score)]
         return computed_value
@@ -42,7 +42,6 @@ class RebaTableManager(TableManager):
 
     def get_tableC_score(self, score_a, score_b):
         computed_value = self.tableC[score_b][score_a]
-        print("Table C: " + str(computed_value))
         return computed_value
 
     def create_table(self, table):
@@ -68,8 +67,12 @@ class RulaTableManager(TableManager):
         self.tableB = self.create_table(2)
         self.tableC = self.create_table(3)
 
-    def get_tableA_score(self, neck_score, trunk_score, leg_score):
-        pass
+    def get_tableA_score(self, posture_score, upper_score, lower_score, twist_score):
+        upper_score = upper_score - 1
+        lower_score = lower_score - 1
+        computed_value = self.tableA[posture_score][upper_score]
+        computed_value = computed_value[lower_score][str(twist_score)]
+        return computed_value
 
     def get_tableB_score(self, posture_score, neck_score, legs_score):
         computed_value = self.tableB[posture_score][neck_score]
@@ -77,7 +80,11 @@ class RulaTableManager(TableManager):
         return computed_value
 
     def get_tableC_score(self, upper_score, lower_score):
-        pass
+        if upper_score > 6 and lower_score > 7:
+            computed_value = self.tableC[8][str(7)]
+        else:
+            computed_value = self.tableC[lower_score][upper_score]
+        return computed_value
 
     def create_table(self, table):
         real_path = os.path.dirname(os.path.realpath(__file__))
@@ -86,12 +93,10 @@ class RulaTableManager(TableManager):
                 file_dir = os.path.join(real_path, 'tables/RULA-TableA.json')
                 return pd.read_json(file_dir)
             case 2:
-                file_dir = os.path.join(real_path, 'tables/RULA-TableA.json')
+                file_dir = os.path.join(real_path, 'tables/RULA-TableB.json')
                 return pd.read_json(file_dir)
             case 3:
-                file_dir = os.path.join(real_path, 'tables/RULA-TableA.json')
-                return pd.read_json(file_dir)
+                file_dir = os.path.join(real_path, 'tables/RULA-TableC.json')
+                return pd.read_json(file_dir, orient='records')
             case default:
                 return
-
-
